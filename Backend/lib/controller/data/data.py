135b80848feca45cad0648b16bf1d8ac70931581
Sndav Bai <bossstyle@126.com>
2018-10-29 23:28:12 +0800

    small changes
    database + time methods

--- Backend/lib/controller/data/data.py
+++ Backend/lib/controller/data/data.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 from lib.core.database.database import Mysql
 from config.database import DBConfig
-import datetime
+from lib.core.scripts.time import Time
 
 
 class User:
@@ -14,24 +14,54 @@ class User:
         self.db = Mysql(user, password, host, port, db)
         # 加载数据库
 
+    def checkuser(self,username):
+        return len(self.db.select('Users', '', 'username=\''+username+'\''))
+
     def adduser(self, username, password, email, gender):
         # gender M男F女
-        dt=datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")
-        data = {
-            'username': '\'' + username + ' \'',
-            'password': '\'' + password + ' \'',
-            'email': '\'' + email + ' \'',
-            'gender': '\'' + gender + ' \'',
-            'points': 0,
-            'create_time': '\''+dt+'\'',
-            'update_time': '\''+dt+'\''
+        if self.checkuser(username)>0:
+            dt = Time.gettime()
+            data = {
+                'username': '\'' + username + ' \'',
+                'password': '\'' + password + ' \'',
+                'email': '\'' + email + ' \'',
+                'gender': '\'' + gender + ' \'',
+                'points': 0,
+                'create_time': '\''+dt+'\'',
+                'update_time': '\''+dt+'\''
 
+            }
+            return self.db.insert('Users', data)
+        return False
+
+    def changepass(self, uid, password):
+        dt = Time.gettime()
+        data = {
+            'password': '\'' + password + ' \'',
+            'update_time': '\''+dt+'\''
         }
-        return self.db.insert('Users', data)
+        return self.db.update('Users', data, 'user = {0}'.format(uid))
+
+    def changeemail(self, uid, email):
+        dt = Time.gettime()
+        data = {
+            'email': '\'' + email + ' \'',
+            'update_time': '\''+dt+'\''
+        }
+        return self.db.update('Users', data, 'user = {0}'.format(uid))
+
+    # def addpoints(self, uid, points):
+    #     dt = Time.gettime()
+    #     data = {
+    #         'points': 0,
+    #         'update_time': '\''+dt+'\''
+    #     }
+    #     return self.db.update('Users', data, 'user = {0}'.format(uid))
+    # 这里可能是个坑
 
     def changesettings(self, uid, username, password, email, gender):
         # gender M男F女
-        dt=datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+        dt = Time.gettime()
         data = {
             'username': '\'' + username + ' \'',
             'password': '\'' + password + ' \'',
@@ -43,4 +73,3 @@ class User:
 
         }
         return self.db.update('Users', data, 'user = {0}'.format(uid))
-    
---
+++ Backend/lib/core/scripts/time.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+
+
+class Time:
+
+    @staticmethod
+    def gettime():
+        """
+        数据库 datatime 返回模式
+        """
+        return datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+
+
+if __name__ == "__main__":
+    pass
